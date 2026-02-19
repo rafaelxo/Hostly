ee549b789034a902b5f7c38ce9ceb21ca75472f6
LucaLodii <luca.lodi.ll53@gmail.com>
2026-02-19 20:57:44 -0300

    chore: set up the enviroment -Back: hexagonal architecture with Go Front: Atomic Design with React + Ts

---
+++ backend/cmd/server/main.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+func main() {
+	fmt.Println("Starting server on :8080")
+	if err := http.ListenAndServe(":8080", nil); err != nil {
+		log.Fatalf("Server failed to start: %v", err)
+	}
+}
